029d660d9013222de7b0d839a9273b6dbc5cf7a6
Victor Vili <victor.vili.xavier.luna@ccc.ufcg.edu.br>
2025-10-28 17:13:45 -0300

    build: adiciona estrutura inicial de arquivos e diretorios do front

---
+++ frontend/src/main.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import './global.css';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <Router>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+    </Router>
+  </React.StrictMode>
+);
